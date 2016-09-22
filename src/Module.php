2cb4381a65e835b76567486657afe6e9e8fa6547
tofid <andreyklochok@gmail.com>
2016-09-22 19:08:56 +0300

    Added module file

---
+++ src/Module.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace hiqdev\yii2\reminder;
+
+/**
+ * Class Module
+ *
+ * Usage:
+ * 'module' => [
+ *      'reminder' => [
+ *          'class' => '\hiqdev\yii2\reminder\Module',
+ *      ],
+ * ],
+ * ...
+ *
+ * @package hiqdev\yii2\reminder
+ */
+class Module extends \yii\base\Module
+{
+//    public $controllerMap = [
+//        'reminder' => 'hiqdev\yii2\reminder\controllers\ReminderController',
+//    ];
+}
