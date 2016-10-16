53296318177dc8e12a1668e21185a60554d43498
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-10-17 00:14:41 +0300

    Fixed invalid dictionaries usage

--- src/widgets/ReminderButton.php
+++ src/widgets/ReminderButton.php
@@ -34,7 +34,7 @@ class ReminderButton extends Widget
             'actionUrl' => ['/reminder/reminder/create-modal', 'object_id' => $this->object_id],
             'handleSubmit' => Url::toRoute('/reminder/reminder/create'),
             'size' => Modal::SIZE_DEFAULT,
-            'header' => Html::tag('h4', Yii::t('hiqdev/yii2/hipanel/reminder', 'Create new reminder'), ['class' => 'modal-title']),
+            'header' => Html::tag('h4', Yii::t('hiqdev/yii2/reminder', 'Create new reminder'), ['class' => 'modal-title']),
             'toggleButton' => $this->getToggleButton(),
         ]);
     }
@@ -47,7 +47,7 @@ class ReminderButton extends Widget
         return (!empty($this->toggleButton)) ?
             $this->toggleButton :
             [
-                'label' => '<i class="fa fa-bell-o"></i>&nbsp;&nbsp;' . Yii::t('hipanel/reminder', 'Create reminder'),
+                'label' => '<i class="fa fa-bell-o"></i>&nbsp;&nbsp;' . Yii::t('hiqdev/yii2/reminder', 'Create reminder'),
                 'class' => 'btn margin-bottom btn-info pull-right'
             ];
     }
