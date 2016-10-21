4c9336fb48998ed42fdc3ac1d5be34ea38dc492d
tofid <andreyklochok@gmail.com>
2016-10-21 13:35:07 +0300

    Added and populated `description` column

--- src/grid/ReminderGridView.php
+++ src/grid/ReminderGridView.php
@@ -4,7 +4,9 @@ namespace hiqdev\yii2\reminder\grid;
 
 use hipanel\grid\ActionColumn;
 use hipanel\grid\BoxedGridView;
+use hipanel\grid\DataColumn;
 use Yii;
+use yii\helpers\Html;
 
 class ReminderGridView extends BoxedGridView
 {
@@ -13,6 +15,14 @@ class ReminderGridView extends BoxedGridView
         return [
             'periodicity' => [
                 'filter' => false,
+            ],
+            'description' => [
+                'class' => DataColumn::class,
+                'label' => Yii::t('hiqdev/yii2/reminder', 'Description'),
+                'value' => function ($model) {
+                    return Html::a($model->objectLabel, $model->objectLink);
+                },
+                'format' => 'html',
             ],
             'message' => [
                 'filter' => false,
--- src/views/reminder/index.php
+++ src/views/reminder/index.php
@@ -17,38 +17,39 @@ $representation = Yii::$app->request->get('representation');
     'model' => $model,
     'dataProvider' => $dataProvider,
 ]) ?>
-    <?php $page->setSearchFormData(compact([])) ?>
+<?php $page->setSearchFormData(compact([])) ?>
 
-    <?php $page->beginContent('show-actions') ?>
-        <?= $page->renderLayoutSwitcher() ?>
-        <?= $page->renderSorter([
-            'attributes' => [
-                'id',
-            ],
-        ]) ?>
-        <?= $page->renderPerPage() ?>
-    <?php $page->endContent() ?>
+<?php $page->beginContent('show-actions') ?>
+<?= $page->renderLayoutSwitcher() ?>
+<?= $page->renderSorter([
+    'attributes' => [
+        'id',
+    ],
+]) ?>
+<?= $page->renderPerPage() ?>
+<?php $page->endContent() ?>
 
-    <?php $page->beginContent('bulk-actions') ?>
-        <?= $page->renderBulkButton(Yii::t('hipanel', 'Delete'), '/reminder/delete', 'danger') ?>
-    <?php $page->endContent() ?>
+<?php $page->beginContent('bulk-actions') ?>
+<?= $page->renderBulkButton(Yii::t('hipanel', 'Delete'), '/reminder/delete', 'danger') ?>
+<?php $page->endContent() ?>
 
-    <?php $page->beginContent('table') ?>
-        <?php $page->beginBulkForm() ?>
-        <?= ReminderGridView::widget([
-            'boxed' => false,
-            'dataProvider' => $dataProvider,
-            'filterModel' => $model,
-            'tableOptions' => [
-                'class' => 'table table-striped table-bordered table-condensed'
-            ],
-            'columns' => [
-                'checkbox',
-                'periodicity', 'next_time',
-                'actions',
-            ],
-        ]) ?>
-        <?php $page->endBulkForm() ?>
-    <?php $page->endContent() ?>
+<?php $page->beginContent('table') ?>
+<?php $page->beginBulkForm() ?>
+<?= ReminderGridView::widget([
+    'boxed' => false,
+    'dataProvider' => $dataProvider,
+    'tableOptions' => [
+        'class' => 'table table-striped table-bordered table-condensed'
+    ],
+    'columns' => [
+        'checkbox',
+        'description',
+        'periodicity',
+        'next_time',
+        'actions',
+    ],
+]) ?>
+<?php $page->endBulkForm() ?>
+<?php $page->endContent() ?>
 <?php $page->end() ?>
 <?php Pjax::end() ?>
