37c3a1c7dd0622f563d427c7a3a24fca4f08e6b5
tofid <andreyklochok@gmail.com>
2016-10-21 19:52:23 +0300

    Fixed Delete button url

--- src/views/reminder/index.php
+++ src/views/reminder/index.php
@@ -18,7 +18,7 @@ $representation = Yii::$app->request->get('representation');
 <?php $page->endContent() ?>
 
 <?php $page->beginContent('bulk-actions') ?>
-<?= $page->renderBulkButton(Yii::t('hipanel', 'Delete'), '/reminder/delete', 'danger') ?>
+<?= $page->renderBulkButton(Yii::t('hipanel', 'Delete'), '/reminder/reminder/delete', 'danger') ?>
 <?php $page->endContent() ?>
 
 <?php $page->beginContent('table') ?>
