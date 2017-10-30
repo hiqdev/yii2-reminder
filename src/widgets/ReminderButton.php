196988771d194999298ed5792ecf75eb43df0973
tofid <andreyklochok@gmail.com>
2017-10-30 13:44:56 +0200

    Changed ReminderButton widget. Render modal markup on `event_end_body`

--- src/widgets/ReminderButton.php
+++ src/widgets/ReminderButton.php
@@ -18,6 +18,7 @@ use yii\base\Widget;
 use yii\bootstrap\Modal;
 use yii\helpers\Html;
 use yii\helpers\Url;
+use yii\web\View;
 
 class ReminderButton extends Widget
 {
@@ -25,27 +26,40 @@ class ReminderButton extends Widget
 
     public $toggleButton = [];
 
+    public $toggleButtonOptions = [];
+
     public function init()
     {
         $this->registerClientScript();
+
+        $this->view->on(View::EVENT_END_BODY, function ($event) {
+            echo AjaxModal::widget([
+                'bulkPage' => false,
+                'id' => $this->getModalId(),
+                'successText' => Yii::t('hiqdev:yii2:reminder', 'Reminder created'),
+                'modalFormId' => 'reminder-form-' . $this->object_id,
+                'scenario' => 'create',
+                'actionUrl' => ['/reminder/reminder/create-modal', 'object_id' => $this->object_id],
+                'handleSubmit' => Url::toRoute('/reminder/reminder/create'),
+                'size' => Modal::SIZE_DEFAULT,
+                'header' => Html::tag('h4', Yii::t('hiqdev:yii2:reminder', 'Create new reminder'), ['class' => 'modal-title']),
+                'toggleButton' => false,
+            ]);
+        });
     }
 
     public function run()
     {
-        $modalId = 'reminder-modal-' . $this->object_id;
-        $this->getView()->registerCss("button[data-target='#{$modalId}'] {margin-top: -3px;}");
-        return AjaxModal::widget([
-            'bulkPage' => false,
-            'id' => $modalId,
-            'successText' => Yii::t('hiqdev:yii2:reminder', 'Reminder created'),
-            'modalFormId' => 'reminder-form-' . $this->object_id,
-            'scenario' => 'create',
-            'actionUrl' => ['/reminder/reminder/create-modal', 'object_id' => $this->object_id],
-            'handleSubmit' => Url::toRoute('/reminder/reminder/create'),
-            'size' => Modal::SIZE_DEFAULT,
-            'header' => Html::tag('h4', Yii::t('hiqdev:yii2:reminder', 'Create new reminder'), ['class' => 'modal-title']),
-            'toggleButton' => $this->getToggleButton(),
-        ]);
+        $modalId = $this->getModalId();
+        $this->view->registerCss("button[data-target='#{$modalId}'] {margin-top: -3px;}");
+
+        return Html::button('<i class="fa fa-bell-o"></i>&nbsp;' . Yii::t('hiqdev:yii2:reminder', 'Create reminder'), array_merge([
+            'class' => 'btn margin-bottom btn-info btn-xs pull-right',
+            'data' => [
+                'toggle' => 'modal',
+                'target' => "#{$modalId}",
+            ],
+        ], $this->toggleButtonOptions));
     }
 
     /**
@@ -74,5 +88,10 @@ class ReminderButton extends Widget
         $view = $this->getView();
         MomentAsset::register($view);
         BootstrapDatetimepickerAsset::register($view);
+    }
+
+    private function getModalId()
+    {
+        return 'reminder-modal-' . $this->object_id;
     }
 }
