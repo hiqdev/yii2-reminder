5c02c268fd6523f4b7ca581b26a3d3b743ac78bf
gadezist <andrybitko@gmail.com>
2022-09-30 16:07:52 +0300

    HP-1037 Remove PJAX form HiPanel

--- src/views/reminder/index.php
+++ src/views/reminder/index.php
@@ -1,8 +1,14 @@
 <?php
 
 use hipanel\widgets\IndexPage;
-use hipanel\widgets\Pjax;
+use hiqdev\hiart\ActiveDataProvider;
 use hiqdev\yii2\reminder\grid\ReminderGridView;
+use yii\base\Model;
+
+/**
+ * @var Model $model
+ * @var ActiveDataProvider $dataProvider
+ */
 
 $this->title = Yii::t('hiqdev:yii2:reminder', 'Reminders');
 $this->params['subtitle'] = array_filter(Yii::$app->request->get($model->formName(), [])) ? Yii::t('hipanel', 'filtered list') : Yii::t('hipanel', 'full list');
@@ -10,33 +16,32 @@ $this->params['breadcrumbs'][] = $this->title;
 
 $representation = Yii::$app->request->get('representation');
 ?>
-<?php Pjax::begin(array_merge(Yii::$app->params['pjax'], ['enablePushState' => true])) ?>
-    <?php $page = IndexPage::begin(['model' => $model, 'dataProvider' => $dataProvider]) ?>
 
-    <?php $page->setSearchFormData() ?>
+<?php $page = IndexPage::begin(['model' => $model, 'dataProvider' => $dataProvider]) ?>
 
-    <?php $page->beginContent('bulk-actions') ?>
-        <?= $page->renderBulkDeleteButton('/reminder/reminder/delete') ?>
-    <?php $page->endContent() ?>
+<?php $page->setSearchFormData() ?>
 
-    <?php $page->beginContent('table') ?>
-        <?php $page->beginBulkForm() ?>
-            <?= ReminderGridView::widget([
-                'boxed' => false,
-                'dataProvider' => $dataProvider,
-                'tableOptions' => [
-                    'class' => 'table table-striped table-bordered table-condensed',
-                ],
-                'columns' => [
-                    'checkbox',
-                    'actions',
-                    'periodicity',
-                    'object',
-                    'type',
-                    'next_time',
-                ],
-            ]) ?>
-        <?php $page->endBulkForm() ?>
-    <?php $page->endContent() ?>
-    <?php $page->end() ?>
-<?php Pjax::end() ?>
+<?php $page->beginContent('bulk-actions') ?>
+    <?= $page->renderBulkDeleteButton('/reminder/reminder/delete') ?>
+<?php $page->endContent() ?>
+
+<?php $page->beginContent('table') ?>
+    <?php $page->beginBulkForm() ?>
+        <?= ReminderGridView::widget([
+            'boxed' => false,
+            'dataProvider' => $dataProvider,
+            'tableOptions' => [
+                'class' => 'table table-striped table-bordered table-condensed',
+            ],
+            'columns' => [
+                'checkbox',
+                'actions',
+                'periodicity',
+                'object',
+                'type',
+                'next_time',
+            ],
+        ]) ?>
+    <?php $page->endBulkForm() ?>
+<?php $page->endContent() ?>
+<?php $page->end() ?>
